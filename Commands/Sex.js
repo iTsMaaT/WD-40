2df9bf08bc04616d6273d95e3450074b56eae478
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-10 12:33:42 -0500

    Fixed perms

--- Commands/Sex.js
+++ Commands/Sex.js
@@ -3,7 +3,7 @@ import AbstractCommand from "../AbstractCommand.js";
 //starts from a random number, then counts everytime a precise user says sex, no args to reset
 export default class Sex extends AbstractCommand{
     execute(message,args) {
-        if (message.member.permissions.has("MentionEveryone")) {
+        if (message.member.permissions.has("Administrator")) {
             if (args.length == 1) {
                 SexID = args[0];
                 message.channel.send (`\<\@${SexID}\> is too horny`);
