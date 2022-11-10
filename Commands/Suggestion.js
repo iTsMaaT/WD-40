f14198ac83ffa1741b70a9e835c500fe19154fc6
iTsMaaT <102565513+iTsMaaT@users.noreply.github.com>
2022-11-10 15:57:49 -0500

    Added spacing in suggestion

--- Commands/Suggestion.js
+++ Commands/Suggestion.js
@@ -2,5 +2,6 @@ import AbstractCommand from "../AbstractCommand.js";
 export default class Suggestion extends AbstractCommand{
     execute(message,args) {
         client.channels.cache.get("1040076894932062229").send(args.join(' '));
+        client.channels.cache.get("1040076894932062229").send(`_ _`);
     }
 }
