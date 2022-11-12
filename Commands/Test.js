9cb7c55b79f47ab3d1f2a68f15eadb0c30e80d0c
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-12 17:30:18 -0500

    nothing

--- Commands/Test.js
+++ Commands/Test.js
@@ -3,7 +3,6 @@ import AbstractCommand from "../AbstractCommand.js";
 //test command
 export default class Test extends AbstractCommand{
     execute(message,args) {
-            message.channel.send({content:"Shut the fuck up" , ephemeral: true});
             message.channel.send("Shut the fuck up");
     }
 }
