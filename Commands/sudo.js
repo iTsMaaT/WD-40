0f5954806294d441e5bf245c5f4f18390eefd100
iTsMaaT <matoulimar1@gmail.com>
2022-11-13 19:03:55 -0500

    Fixed stupid slash command

--- Commands/sudo.js
+++ Commands/sudo.js
@@ -3,20 +3,9 @@ module.exports={
     description:"idk",
     execute(client,message,args) {
         if (message.author.id == 411996978583699456 && args.length > 1) {
-            //if (args[0] == "-s") {
                 const SudoID = args[0];
                 args[0] = "";
-                //args[1] = "";
                 client.channels.cache.get(SudoID).send(args.join(' '));
-            //}
-            /*else if(args[0] == "-r") {
-                const SudoID = args[1];
-                args[0] = "";
-                args[1] = "";
-                client.guilds.cache.get(SudoID).messages.fetch(MsgID).then(async m => {
-                    await m.reply(args.join(' '));
-                });
-            }*/
         }
         console.log("Sudo used");
     }
--- slash/ping.js
+++ slash/ping.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const {prettyMilliseconds} = require("pretty-ms");
+const prettyMilliseconds = require("pretty-ms");
 const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,15 +8,12 @@ module.exports = {
 	async execute(interaction, client) {
         await interaction.deferReply()
 		const sent = await interaction.editReply({ content: 'Pinging...', fetchReply: true });
-		wait(4000)
-		const embed = new EmbedBuilder()
-        .setTitle("Bot latency")
-        .addFields(
-            {name:"Bot latency", value:`${client.ws.ping}` + 'ms'},
-            {name:"Bot uptime", value:`${prettyMilliseconds(client.uptime)}`},
-			{name:"Round trip latency", value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`}
-            )
-        .setTimestamp()
-		await interaction.editReply({embeds:[embed]});
+    
+        interaction.editReply(`
+
+My ping is \`${client.ws.ping}ms\`
+Uptime : \`${prettyMilliseconds(client.uptime)}\`
+Round trip latency : \`${sent.createdTimestamp - interaction.createdTimestamp}ms\`
+            `);
 	},
 };
