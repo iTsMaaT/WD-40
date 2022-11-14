6d16aff6f894cfe45db9b9c8400188d0c0fa30cb
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-13 23:35:49 -0500

    activity command added

---
+++ Commands/activity.js
@@ -0,0 +1,14 @@
+module.exports={
+    name:"Activity",
+    description:"Changes the bot's activity status",
+    execute(client,message,args) {
+        if (message.author.id == 411996978583699456 && args.length != 0) {
+            client.user.setActivity(args.join(' '));
+            message.reply("Activity changed.");
+        }
+        else {
+            client.user.setActivity(`Time to be annoying!`);
+            message.reply("Activity set to default");
+        }
+    }
+}
