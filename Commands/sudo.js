975ca385c0056681608c131b386d68899d515aaa
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-13 19:48:28 -0500

    sudo -r

--- Commands/sudo.js
+++ Commands/sudo.js
@@ -3,9 +3,16 @@ module.exports={
     description:"idk",
     execute(client,message,args) {
         if (message.author.id == 411996978583699456 && args.length > 1) {
-                const SudoID = args[0];
-                args[0] = "";
+            let sudoprefix = args.shift();
+            if (sudoprefix == "-s") {
+                const SudoID = args.shift();
                 client.channels.cache.get(SudoID).send(args.join(' '));
+            }
+            else if (sudoprefix == "-r") {
+                const ChannelID = args.shift();
+                const MsgID = args.shift();
+                client.channels.cache.get(ChannelID).messages.cache.get(MsgID).fetch().then(m => m.reply(args.join(' ')));
+            }
         }
         console.log("Sudo used");
     }
