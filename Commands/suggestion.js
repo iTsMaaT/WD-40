5fbb1c62842e78641897aab30fba220faba53751
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-17 10:16:43 -0500

    Made it look better ig

--- Commands/suggestion.js
+++ Commands/suggestion.js
@@ -2,7 +2,7 @@ module.exports={
     name:"suggestion",
     description:"suggest",
     execute(client,message,args) {
-        client.channels.cache.get("1040076894932062229").send(args.join(' '));
+        client.channels.cache.get("1040076894932062229").send("Suggestion received:" + args.join(' '));
         client.channels.cache.get("1040076894932062229").send(`- - - - - - - - - - -`);
         message.reply(`Suggestion received.`);
         console.log(`Suggestion received`);
