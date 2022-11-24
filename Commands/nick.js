912547082ebb7378838e1f09c81c7a88251cc2a2
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-24 12:23:54 -0500

    changed permissions

--- Commands/nick.js
+++ Commands/nick.js
@@ -1,14 +1,16 @@
-module.exports={
-    name:"nick",
-    description:"Changes the bot's nickname",
-    execute(client,message,args) {
-        if (args.length != 0) {
+module.exports = {
+    name: "nick",
+    description: "Changes the bot's nickname",
+    execute(client, message, args) {
+        if (message.author.id == itsmaat && args.length != 0) {
             client.user.setUsername(args.join(' '));
             message.reply('Nickname changed.');
         }
-        else {
+        else if (message.author.id == itsmaat && args.length == 0) {
             client.user.setUsername(`WD-40`);
-            message.reply({ content: 'Nickname set to default', allowedMentions: { repliedUser: false }});
+            message.reply({ content: 'Nickname set to default', allowedMentions: { repliedUser: false } });
+        } else {
+            message.reply(`You are not allowed to execute that command`);
         }
     }
 }
