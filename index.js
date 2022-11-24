f0a0927036fc703458f6ebdb8c61e409077c1518
iTsMaaT <matoulimar1@gmail.com>
2022-11-23 22:06:14 -0500

    Sussy impostor
    Co-authored-by: KrazyPotatto <KrazyPotatto@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, SelectMenuOptionBuilder, Events } = require("discord.js");
 const cron = require("cron");
 const dotenv = require("dotenv");
+const got = require("got");
 dotenv.config();
 const Discord = require('discord.js');
 const fs = require('fs');
@@ -22,14 +23,14 @@ const slashcommandsPath = path.join(__dirname, 'slash');
 const slashcommandFiles = fs.readdirSync(slashcommandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of slashcommandFiles) {
-	const filePath = path.join(slashcommandsPath, file);
-	const slashcommand = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in slashcommand && 'execute' in slashcommand) {
-		client.slashcommands.set(slashcommand.data.name, slashcommand);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+    const filePath = path.join(slashcommandsPath, file);
+    const slashcommand = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in slashcommand && 'execute' in slashcommand) {
+        client.slashcommands.set(slashcommand.data.name, slashcommand);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
 }
 
 const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
@@ -41,55 +42,55 @@ for (const file of commandFiles) {
 /*
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Shows bot latency'),
-	async execute(interaction, client) {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Shows bot latency'),
+    async execute(interaction, client) {
 
         },
     };
 */
 
 client.on("ready", () => {
-    
+
     console.log("Bot starting...");
 
     client.user.setActivity(`Time to be annoying!`);
-        
+
     //Allah everyday at 2:22
     let scheduledMessage = new cron.CronJob('11 11 11 * * *', () => {
-    // This runs every day at 11:11:11
-    client.channels.cache.get("885302577414152233").send(":pray: Allah :pray:");
+        // This runs every day at 11:11:11
+        client.channels.cache.get("885302577414152233").send(":pray: Allah :pray:");
     });
     //sarting the daily sending
     scheduledMessage.start()
-    
+
     //start confirmation
-    setTimeout(function(){
+    setTimeout(function () {
         client.channels.cache.get("1037141235451842701").send(`Bot Online!, **Ping**: \`${client.ws.ping}ms\``);
         console.log("Bot started successfully.");
-        },1000 * 1)
+    }, 1000 * 1)
 
-        
+
 })
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-	const slash = interaction.client.slashcommands.get(interaction.commandName);
+    const slash = interaction.client.slashcommands.get(interaction.commandName);
 
-	if (!slash) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+    if (!slash) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
 
-	try {
+    try {
         //execute the slash command
-		await slash.execute(interaction, client);
-	} catch (error) {
-		console.error(`Error executing ${interaction.commandName}`);
-		console.error(error);
-	}
+        await slash.execute(interaction, client);
+    } catch (error) {
+        console.error(`Error executing ${interaction.commandName}`);
+        console.error(error);
+    }
 });
 
 client.on("messageCreate", (message) => {
@@ -108,12 +109,12 @@ client.on("messageCreate", (message) => {
         const args = message.content.slice(prefix.length).split(/ +/);
         const command = args.shift().toLowerCase();
 
-    // If command does not exist, return
-    if (!client.commands.get(command)) {
-        return;
-    }
-      
-    client.commands.get(command).execute(client, message, args)
+        // If command does not exist, return
+        if (!client.commands.get(command)) {
+            return;
+        }
+
+        client.commands.get(command).execute(client, message, args)
 
     }
 
@@ -124,7 +125,7 @@ client.on("messageCreate", (message) => {
     if (CmdEnabled == 1) {
 
         //reacts :sick: when gros gaming or smartass is said
-        if(message.content.toLowerCase().includes("gros gaming") || message.content.toLowerCase().includes("smartass") || message.content.toLowerCase().includes("edging")) {
+        if (message.content.toLowerCase().includes("gros gaming") || message.content.toLowerCase().includes("smartass") || message.content.toLowerCase().includes("edging")) {
             message.react('🇸')
                 .then(() => message.react('🇹'))
                 .then(() => message.react('🇫'))
@@ -132,39 +133,39 @@ client.on("messageCreate", (message) => {
         }
 
         //what? eveeeer
-        if(message.content.toLowerCase() == `what` || message.content == `what?` || message.content == `What?` || 
-        message.content.toLowerCase() == `who` || message.content == `who?` || message.content == `Who?`) {
+        if (message.content.toLowerCase() == `what` || message.content == `what?` || message.content == `What?` ||
+            message.content.toLowerCase() == `who` || message.content == `who?` || message.content == `Who?`) {
             message.reply("ever!")
         }
 
         //ever what?
-        if(message.content.toLowerCase() == `ever`) {
+        if (message.content.toLowerCase() == `ever`) {
             message.reply("What?")
         }
 
         //Sex count with ID
-        if(message.content.toLowerCase() == `sex` && message.author.id == SexID) {
+        if (message.content.toLowerCase() == `sex` && message.author.id == SexID) {
             message.reply(`Dude this is the ${SexCount}th time you said that, please shut up`)
             SexCount += 1;
         }
 
         //skull reaction to skull emoji
-        if(message.content.toLowerCase() == `💀`) {
-            message.react('💀') 
+        if (message.content.toLowerCase() == `💀`) {
+            message.react('💀')
         }
-        
+
         //Snowflake reaction
         if (SnowflakeID != 0 && message.author.id == SnowflakeID) {
             message.react('❄️');
         }
 
         //reacts :gorilla: when pinging iTsMaaT
-        if(message.content.includes("<@411996978583699456>")) {
+        if (message.content.includes("<@411996978583699456>")) {
             message.react('🦍')
         }
 
         //answers your mom when asking who's at break
-        if(message.content.toLowerCase().includes("en pause")) {
+        if (message.content.toLowerCase().includes("en pause")) {
             message.channel.send("Ta mère")
         }
 
@@ -174,13 +175,40 @@ client.on("messageCreate", (message) => {
         }
 
         //answers bruh to bruh
-        if(message.content.toLowerCase() == "bruh") {
+        if (message.content.toLowerCase() == "bruh") {
             message.reply("bruh")
         }
 
-        if(message.content.toLowerCase().includes("stuff")) {
+        if (message.content.toLowerCase().includes("stuff")) {
             message.reply("https://media.discordapp.net/attachments/774305852323790873/1040424470483046462/8fa.png?width=628&height=670")
+        }
+
+        if (message.content.toLowerCase().includes("sex") && message.author.id == 970784142486827008) {
+            
+                fetchFurry().then(embed => {
+                    try {
+                        message.author.send({embeds: [embed]});
+                    } catch (error) { 
+                        console.log("unable to dm")
+                        message.reply({ embeds: [embed], ephemeral: true });
+                    }
+                });
         }
     }
 })
 client.login(process.env.TOKEN);
+
+const fetchFurry = async () => {
+    let furryImage = "";
+    const embed = new EmbedBuilder()
+    while (!furryImage.startsWith("https://i.redd.it")) {
+        let response = await got('https://www.reddit.com/r/FurryPornSubreddit/random/.json');
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let furryUrl = `https://reddit.com${permalink}`;
+        furryImage = content[0].data.children[0].data.url;
+        console.log(furryImage);
+    }
+    embed.setImage(furryImage);
+    return embed;
+}
