2a4b951b38b9b0b7d84512e78c52c3665ad48d60
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-25 15:41:33 -0500

    mentions by who the suggestion was made (and gives username + discriminator)

--- Commands/suggestion.js
+++ Commands/suggestion.js
@@ -2,7 +2,8 @@ module.exports = {
     name: "suggestion",
     description: "suggest",
     execute(client, message, args) {
-        client.channels.cache.get("1040076894932062229").send("**Suggestion received: **" + args.join(' '));
+        
+        client.channels.cache.get("1040076894932062229").send(`**Suggestion by ${message.author} (${message.member.user.tag}) received: **` + args.join(' '));
         client.channels.cache.get("1040076894932062229").send(`- - - - - - - - - - -`);
         message.reply(`Suggestion received.`);
         console.log(`Suggestion received`);
