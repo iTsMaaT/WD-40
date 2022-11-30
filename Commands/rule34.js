97126e5d58c0ece5202ac7ecfa7d5bb9039f935a
Mathis Oulimar <matoulimar1@gmail.com>
2022-11-29 22:39:42 -0500

    made it word with reddit

---
+++ Commands/rule34.js
@@ -0,0 +1,68 @@
+const got = require("got");
+module.exports = {
+    name: "rule34",
+    description: "yes",
+    execute: async (client, message, args) => {
+        switch (Math.floor(Math.random() * 5 + 1)) {
+            case 1:
+                var RuleLink = "https://www.reddit.com/r/rule34/random/.json";
+                console.log("R34")
+                break;
+            case 2:
+                var RuleLink = "https://www.reddit.com/r/rule34lol/random/.json";
+                console.log("R34lol")
+                break;
+            case 3:
+                var RuleLink = "https://www.reddit.com/r/rule34rainbowsix/random/.json";
+                console.log("R34R6")
+                break;
+            case 4:
+                var RuleLink = "https://www.reddit.com/r/rule34feet/random/.json";
+                console.log("R34feet")
+                break;
+            case 5:
+                var RuleLink = "https://www.reddit.com/r/2booty/random/.json";
+                console.log("R34NierAutomata")
+                break;
+        }
+        try {
+            let RuleImage = "";
+            while (!RuleImage.startsWith("https://i.redd.it")) {
+                let response = await got(RuleLink);
+                let content = JSON.parse(response.body);
+                let permalink = content[0].data.children[0].data.permalink;
+                var RulePostUrl = `https://reddit.com${permalink}`;
+                RuleImage = content[0].data.children[0].data.url;
+            }
+            message.reply({
+                content: `||<${RulePostUrl}>||`,
+                files: [{
+                    attachment: RuleImage,
+                    name: `SPOILER_FILE.jpg`
+                }]
+            });
+        }
+        catch (err) {
+            try {
+                while (!RuleImage.startsWith("https://i.redd.it")) {
+                    let response = await got("https://www.reddit.com/r/rule34/random/.json");
+                    let content = JSON.parse(response.body);
+                    let permalink = content[0].data.children[0].data.permalink;
+                    var RulePostUrl = `https://reddit.com${permalink}`;
+                    RuleImage = content[0].data.children[0].data.url;
+                }
+                message.reply({
+                    content: `||<${RulePostUrl}>||`,
+                    files: [{
+                        attachment: RuleImage,
+                        name: `SPOILER_FILE.jpg`
+                    }]
+                });
+                console.log(err);
+            }
+            catch (err) {
+                message.reply(`Error while making the file as spoiler, please try again.\n\`${err}\``)
+            }
+        }
+    }
+}
