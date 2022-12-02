7094c349a7c9f2ce93bee1d94081e05390d362af
Mathis Oulimar <matoulimar1@gmail.com>
2022-12-02 12:15:45 -0500

    chnaged easter eggs

--- Commands/serverinfo.js
+++ Commands/serverinfo.js
@@ -3,31 +3,22 @@ module.exports = {
     description: "Gives info of a user",
     execute: async(client, message, args) => {
         const owner = await message.guild.fetchOwner();
-        const rep = message.reply({
+        message.reply({
             content: `
 __General__
 **Server name**: \`${message.guild.name}\`
     
-**Created**: <t:${parseInt(message.guild.createdTimestamp / 1000)}:R>
+**Created at**: <t:${parseInt(message.guild.createdTimestamp / 1000)}:D> (<t:${parseInt(message.guild.createdTimestamp / 1000)}:R>)
 **Owner**: ${owner}
 **Server description**: ${message.guild.description}
 
 __Users__
 **User count**: ${message.guild.members.cache.filter((m) => !m.user.bot).size}
 **Bot count**: ${message.guild.members.cache.filter((m) => m.user.bot).size}
-**Total**: ${message.guild.members.cache.filter((m) => m.user).size}
+**Total**: ${message.guild.members.cache.filter((m) => m.user).size} ${ message.guild.members.cache.filter((m) => m.user).size == 69 ? "(Nice)" : ""}  
 
 ${ message.guild.members.cache.filter((m) => m.user).size == 42 ? "This is the answer to life!" : ""}    
 `, allowedMentions: { repliedUser: false }
         })
-        if (message.guild.members.cache.filter((m) => m.user).size == 69) {
-            rep.then(m => {
-                m.react('🇳')
-                .then(() => m.react('🇮'))
-                .then(() => m.react('🇨'))
-                .then(() => m.react('🇪'));
-            });
-        }
-        
     }
 }
