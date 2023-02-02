2a74f1b44ef9a9621bdbff7c269fe47e16541bac
iTsMaaT <matoulimar1@gmail.com>
2023-02-02 12:56:42 -0500

    fixed

--- Commands/restart.js
+++ Commands/restart.js
@@ -7,9 +7,12 @@ module.exports = {
             logger.severe("Restart requested from discord...");
             logger.severe("Bot crash incoming");
             client.channels.cache.get("1037141235451842701").send("Restart requested from discord...");
+            
+            setTimeout(function () {
             /****************/
             client.channels.cache.get().send();
             /****************/
+            }, 1000 * 1)
         }
         else if (message.member.permissions.has("Administrator")) {
             message.channel.send(`Please contact the owner of this bot to execute this command (iTsMaaT#4020 or <@411996978583699456>)`);
