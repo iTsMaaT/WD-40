312091527f0fa8aeaf92336e477764bfa0802f5c
iTsMaaT <matoulimar1@gmail.com>
2023-02-02 12:51:50 -0500

    restart from discord

---
+++ Commands/restart.js
@@ -0,0 +1,21 @@
+const prettyMilliseconds = require('pretty-ms');
+module.exports = {
+    name: "restart",
+    description: "restart the bot from discord (intentionally crashes)",
+    execute(logger, client, message, args) {
+        if (message.author.id == 411996978583699456) {
+            logger.severe("Restart requested from discord...");
+            logger.severe("Bot crash incoming");
+            client.channels.cache.get("1037141235451842701").send("Restart requested from discord...");
+            /****************/
+            client.channels.cache.get().send();
+            /****************/
+        }
+        else if (message.member.permissions.has("Administrator")) {
+            message.channel.send(`Please contact the owner of this bot to execute this command (iTsMaaT#4020 or <@411996978583699456>)`);
+        }
+        else {
+            message.channel.send(`nah`);
+        }
+    }
+}
