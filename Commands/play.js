c07756c0e1a4af658e468483c68cf1c435e71ec8
iTsMaaT <matoulimar1@gmail.com>
2023-02-03 23:50:40 -0500

    music?

---
+++ Commands/play.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder } = require("discord.js")
+module.exports = {
+  name: "play",
+  description: "Play a song",
+  execute(logger, client, message, args) {
+    console.log(message);
+    if (!message.member.voice.channel) {
+      
+      const must_be_in_vc_embed = new EmbedBuilder()
+        .setColor("#FF0000")
+        .setDescription(`You must be in a voice channel!`)
+        .setTimestamp()
+      return message.channel.send({ embeds: [must_be_in_vc_embed] })
+    }
+    const string = args.join(' ')
+    if (!message.member.voice.channel) {
+      const must_be_in_vc_embed = new EmbedBuilder()
+        .setColor("#FF0000")
+        .setDescription(`You must be in a voice channel!`)
+        .setTimestamp()
+      return message.channel.send({ embeds: [must_be_in_vc_embed] })
+    }
+    const no_query_embed = new EmbedBuilder()
+      .setColor("#FF0000")
+      .setDescription(`Please enter a song URL or query to search.`)
+      .setTimestamp()
+    if (!string) return message.channel.send({ embeds: [no_query_embed] })
+    client.distube.play(message.member.voice.channel, string, {
+      member: message.member,
+      textChannel: message.channel,
+      message
+    })
+  }
+}
