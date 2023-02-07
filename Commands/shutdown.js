0ee7af008d2a451fc49e39b9f791be2695b34e2f
iTsMaaT <matoulimar1@gmail.com>
2023-02-06 22:45:45 -0500

    shutdown

--- Commands/shutdown.js
+++ Commands/shutdown.js
@@ -8,6 +8,7 @@ module.exports = {
             client.channels.cache.get("1037141235451842701").send(`Bot shutdown requested, **Uptime**: \`${prettyMilliseconds(client.uptime)}\``);
             message.channel.send(`**Shuting down the bot...**\n**Uptime**: \`${prettyMilliseconds(client.uptime)}\``).then(() => {
                 client.destroy();
+                process.exit(0);
             })
         }
         else if (message.member.permissions.has("Administrator")) {
