f87c48520100e88b6c84d720b4bc15b04c54ac00
iTsMaaT <matoulimar1@gmail.com>
2023-04-22 21:07:18 -0400

    Lists the guilds

---
+++ Commands/utils/guilds.js
@@ -0,0 +1,20 @@
+const USERID = require("../../UserIDs.js");
+module.exports = {
+    name: "guilds",
+    category: "utils",
+    description: "Makes a list of the guilds the bot is in",
+    private: true,
+    execute(logger, client, message, args) {
+        if (message.author.id == USERID.itsmaat) {
+            let content = client.guilds.cache.map(guild => `${guild.name} (\`${guild.id}\`)`).join("\n");
+            message.reply({ content, allowedMentions: { repliedUser: false } });
+            logger.info("The bot is in :" + content)
+
+            /* Antoine stupide (it works ig)
+            console.log(client.guilds.cache.map(guild => guild.name).reduce((previous,current) => {
+                return previous + ", " + current
+            }, "").substring(2))
+            */
+        }
+    }
+}
