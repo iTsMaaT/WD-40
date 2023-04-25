012caaa57db1441a12993281353affa9923b80ee
iTsMaaT <matoulimar1@gmail.com>
2023-04-24 21:07:14 -0400

    placeholders and adding guild logging

---
+++ Commands/Z] placeholder/superuser.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "superuser",
+    description: "Placeholder to log the command",
+    category: "placeholders",
+    private: true,
+    execute(logger, client, message, args) {
+
+    }
+}
