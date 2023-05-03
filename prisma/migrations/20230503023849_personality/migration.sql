68ed5be6748af1b64e604502df05175d9f05d06b
iTsMaaT <matoulimar1@gmail.com>
2023-05-02 22:41:18 -0400

    fix

--- prisma/migrations/20230503023849_personality/migration.sql
+++ prisma/migrations/20230503023849_personality/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `GuildGettings` ADD COLUMN `Personality` VARCHAR(500) NOT NULL DEFAULT 'Answer as if you are annoying.';
+ALTER TABLE `GuildSettings` ADD COLUMN `Personality` VARCHAR(500) NOT NULL DEFAULT 'Answer as if you are annoying.';
