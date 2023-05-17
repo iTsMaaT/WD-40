04bd15c3ea13c9e1177dd3973d63e4722861f35e
iTsMaaT <matoulimar1@gmail.com>
2023-05-17 17:33:13 -0400

    Initial commit

---
+++ utils/functions/RandomMinMax.js
@@ -0,0 +1,8 @@
+
+/**
+ * Returns a random Integer between @param min and @param max
+ */
+const RandomMinMax = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+module.exports = RandomMinMax;
