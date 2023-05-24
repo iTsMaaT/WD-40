d8cb657587f7cb192073f9a9765b9fdddb12a77d
iTsMaaT <matoulimar1@gmail.com>
2023-05-23 21:41:59 -0400

    Akinator

---
+++ Commands/fun/fun/guess.js
@@ -0,0 +1,15 @@
+const akinator = require("discord.js-akinator");
+module.exports = {
+    name: "guess",
+    description: "Guess a character with akinator",
+    category: "fun",
+    execute: async(logger, client, message, args) =>{
+        akinator(message, {
+            language: "en", // Defaults to "en"
+            childMode: false, // Defaults to "false"
+            gameType: "character", // Defaults to "character"
+            useButtons: true, // Defaults to "false"
+            embedColor: '#ffffff' // Defaults to "Random"
+        });
+    }
+}
