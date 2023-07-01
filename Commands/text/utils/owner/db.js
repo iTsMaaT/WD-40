69a2b54945c1a5e99c77f4b7ca830f10744dd084
iTsMaaT <matoulimar1@gmail.com>
2023-06-30 22:24:13 -0400

    initial commit

---
+++ Commands/text/utils/owner/db.js
@@ -0,0 +1,59 @@
+const fs = require('fs/promises');
+
+module.exports = {
+    name: 'db',
+    description: 'Gives db info of a table',
+    category: 'fun',
+    private: true,
+    async execute(logger, client, message, args) {
+        if (message.author.id != process.env.OWNER_ID) return;
+
+        if (!args) {
+            return message.reply('Please provide a table name!');
+        }
+        const tableName = args[0];
+
+        try {
+            const data = await global.prisma[tableName].findMany({
+                orderBy: {
+                    ID: 'desc',
+                },
+                //take: 20,
+            });
+
+            // Calculate the maximum length for each column
+            const columnLengths = {};
+            Object.keys(data[0]).forEach(column => {
+                const maxLength = Math.max(...data.map(row => String(row[column]).length));
+                columnLengths[column] = maxLength;
+            });
+
+            // Create the table header
+            const tableHeader = Object.keys(data[0])
+                .map(header => header.padEnd(columnLengths[header]))
+                .join(' | ');
+
+            // Create the separator line
+            const separatorLine = '-'.repeat(tableHeader.length);
+
+            // Create the table rows
+            const tableRows = data.map(row =>
+                Object.entries(row)
+                    .map(([key, value]) => String(value).padEnd(columnLengths[key]))
+                    .join(' | ')
+            );
+
+            // Join the table header, separator line, and rows
+            const table = [tableHeader, separatorLine, ...tableRows].join('\n');
+
+            // Create a text file
+            const fileName = `./${tableName}_data.txt`;
+            await fs.writeFile(fileName, table, { encoding: 'utf8' });
+            await message.reply({ files: [fileName] });
+            await fs.unlink(fileName);
+        } catch (error) {
+            console.error(error.stack);
+            message.reply('An error occurred while fetching data.');
+        }
+    },
+};
---
+++ Commands/text/utils/owner/source.js
@@ -0,0 +1,25 @@
+const SplitIntoChunks = require("../../../../utils/functions/SplitIntoChunks");
+
+module.exports = {
+    name: "source",
+    category: "utils",
+    description: "Gives the source code of a command",
+    private: true,
+    async execute(logger, client, message, args) {
+        const Whitelist = ['282165267092602880','273587964334964737',process.env.OWNER_ID];
+        if (!Whitelist.includes(message.author.id)) return;
+        const commandName = args[0];
+        const command = client.commands.get(commandName);
+
+        if (!command) return message.channel.send(`Command \`${commandName}\` not found.`);
+
+        const code = command.execute.toString();
+        const chunks = SplitIntoChunks(code);
+
+        await message.reply(`Code for command \`${commandName}\``);
+
+        for (const chunk of chunks) {
+            await message.channel.send(`\`\`\`javascript\n${chunk}\n\`\`\``);
+        }
+    },
+};
