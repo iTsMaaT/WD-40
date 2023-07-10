0db778576ff76312facb58a519ca67cf0547c24c
iTsMaaT <matoulimar1@gmail.com>
2023-07-09 21:29:09 -0400

    initial commit

--- Commands/text/fun/fun/emojify.js
+++ Commands/text/fun/fun/emojify.js
@@ -1,9 +1,78 @@
+const SendErrorEmbed = require("@functions/SendErrorEmbed.js");
+
 module.exports = {
     name: "emojify",
     description: "Transform a string into emotes",
     usage: "< [Prompt] >",
     category: "fun",
     execute(logger, client, message, args) {
+        const emoteMap = {
+            a: '🇦',
+            b: '🇧',
+            c: '🇨',
+            d: '🇩',
+            e: '🇪',
+            f: '🇫',
+            g: '🇬',
+            h: '🇭',
+            i: '🇮',
+            j: '🇯',
+            k: '🇰',
+            l: '🇱',
+            m: '🇲',
+            n: '🇳',
+            o: '🇴',
+            p: '🇵',
+            q: '🇶',
+            r: '🇷',
+            s: '🇸',
+            t: '🇹',
+            u: '🇺',
+            v: '🇻',
+            w: '🇼',
+            x: '🇽',
+            y: '🇾',
+            z: '🇿',
+            0: '0️⃣',
+            1: '1️⃣',
+            2: '2️⃣',
+            3: '3️⃣',
+            4: '4️⃣',
+            5: '5️⃣',
+            6: '6️⃣',
+            7: '7️⃣',
+            8: '8️⃣',
+            9: '9️⃣',
+            '!': '❗',
+            '?': '❓',
+            '#': '#️⃣',
+            '*': '*️⃣',
+            '+': '➕',
+            '-': '➖',
+            '÷': '➗',
+            '$': '💲',
+            '.': '🔵',
+            ',': '🔻',
+            '<': '⬅',
+            '>': '➡',
+            '^': '🔺',
+        };
 
+        if (!args[0]) SendErrorEmbed(message, "You need a prompt", "yellow");
+
+        const emotesPromt = Array.from(args.join(" ").toLowerCase())
+            .map((char) => emoteMap[char] || char) // Use emote mapping or original character if mapping doesn't exist
+            .join(' ');
+
+        if (emotesPromt.length > 1000) SendErrorEmbed(message, "Prompt too long", "yellow");
+
+        const embed = {
+            color: 0xffffff,
+            title: "Emotified text",
+            description: emotesPromt,
+            timestamp: new Date()
+        };
+
+        message.reply({ embeds: [embed] });
     }
 };
--- Commands/text/fun/fun/uwufy.js
+++ Commands/text/fun/fun/uwufy.js
@@ -1,9 +1,41 @@
+const SendErrorEmbed = require("@functions/SendErrorEmbed.js");
+
 module.exports = {
     name: "uwufy",
     description: "UwUfies a prompt",
     usage: "< [Prompt] >",
     category: "fun",
     execute(logger, client, message, args) {
+        
+        if (!args[0]) SendErrorEmbed(message, "You need a prompt", "yellow");
 
+        const UwUpromtpt = uwufyText(args.join(" "));
+        if (UwUpromtpt.length > 1000) SendErrorEmbed(message, "Prompt too long", "yellow");
+
+        const embed = {
+            color: 0xffffff,
+            title: "UwUfied text",
+            description: UwUpromtpt,
+            timestamp: new Date()
+        };
+
+        message.reply({ embeds: [embed] });
+
+
+        function uwufyText(text) {
+            // Replace 'r' or 'l' followed by a vowel with 'w'
+            text = text.replace(/(?:r|l)([aeiou])/gi, 'w$1');
+            
+            // Replace 'n' or 'N' followed by 'a', 'e', 'i', 'o', or 'u' with 'ny'
+            text = text.replace(/n([aeiou])/gi, 'ny$1');
+            
+            // Replace 'ove' with 'uv'
+            text = text.replace(/ove/gi, 'uv');
+            
+            // Add 'uwu' at the end
+            text += ' uwu';
+          
+            return text;
+        }
     }
 };
