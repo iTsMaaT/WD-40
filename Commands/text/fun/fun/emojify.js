17335d070b40c5e918524d785c61f0ee66f19166
iTsMaaT <matoulimar1@gmail.com>
2023-07-09 21:09:20 -0400

    initial commit

---
+++ Commands/text/fun/fun/emojify.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "emojify",
+    description: "Transform a string into emotes",
+    usage: "< [Prompt] >",
+    category: "fun",
+    execute(logger, client, message, args) {
+
+    }
+};
---
+++ Commands/text/fun/fun/uwufy.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "uwufy",
+    description: "UwUfies a prompt",
+    usage: "< [Prompt] >",
+    category: "fun",
+    execute(logger, client, message, args) {
+
+    }
+};
