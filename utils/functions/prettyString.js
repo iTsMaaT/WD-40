fe75ef8f7407ca890744ffa446fbcd9e1de56c0f
iTsMaaT <matoulimar1@gmail.com>
2023-07-23 16:01:27 -0400

    better

--- utils/functions/prettyString.js
+++ utils/functions/prettyString.js
@@ -8,7 +8,7 @@ const prettyString = function (inputString, type, addDot) {
         modifiedString = capitalizedWords.join(" ");
     } else if (type === "first") {
         // Capitalize only the first letter of the entire string
-        modifiedString = inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
+        modifiedString = inputString.charAt(0).toUpperCase() + inputString.slice(1);
     } else {
         modifiedString = inputString; 
     }
