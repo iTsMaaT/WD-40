093ce69b3574947a1b1532847af8996801448173
iTsMaaT <matoulimar1@gmail.com>
2023-08-14 19:01:23 -0400

    embed name fix

--- Commands/text/fun/posts/dog.js
+++ Commands/text/fun/posts/dog.js
@@ -1,31 +1,26 @@
 const got = require('got');
-const SendErrorEmbed = require("@functions/SendErrorEmbed.js");
 
 module.exports = {
     name: 'dog',
     description: 'Not cats!',
     category: "posts",
     async execute(logger, client, message, args) {
-        message.channel.sendTyping();
 
-        if (message.channel.nsfw) {
-            await got("https://dog.ceo/api/breeds/image/random")
-                .then(response => {
-                    var url = JSON.parse(response.body);
+        await got("https://dog.ceo/api/breeds/image/random")
+            .then(response => {
+                var url = JSON.parse(response.body);
 
-                    NekoEmbed = {
-                        color: 0xffffff,
-                        title: `Enjoy!`,
-                        image: {
-                            url: url.message,
-                        },
-                        timestamp: new Date(),
-                    };
+                const embed = {
+                    color: 0xffffff,
+                    title: `Enjoy!`,
+                    image: {
+                        url: url.message,
+                    },
+                    timestamp: new Date(),
+                };
 
-                    message.reply({ embeds: [NekoEmbed] });
-                });
-        } else {
-            return SendErrorEmbed(message, "The channel you are in isn't NSFW", "yellow");
-        }
+                message.reply({ embeds: [embed] });
+            });
+
     }
 };
