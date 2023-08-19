5dd84a706ab14d7214d6cc43bc50ce9a7d136710
iTsMaaT <matoulimar1@gmail.com>
2023-08-19 19:39:42 -0400

    Fixed hanging requests

--- Commands/slash/fun/fun/ask.js
+++ Commands/slash/fun/fun/ask.js
@@ -1,7 +1,4 @@
-const { Configuration, OpenAIApi } = require('openai');
 const { ApplicationCommandType, ApplicationCommandOptionType } = require("discord.js");
-const fs = require("fs/promises");
-const CreateUniqueSeed = require("@functions/CreateUniqueSeed.js");
 
 module.exports = {
     name: "ask",
@@ -9,70 +6,32 @@ module.exports = {
     type: ApplicationCommandType.ChatInput,
     options: [
         {
-            name: "type",
-            description: "The user to check the info from.",
-            type: ApplicationCommandOptionType.String,
-            required: true,
-            choices: [
-                { name: "Change Personality", value: "personality", },
-                { name: "Ask something", value: "prompt", },
-            ],
-        },
-        {
             name: "data",
-            description: "The message to ask or personality",
+            description: "The prompt",
             type: ApplicationCommandOptionType.String,
             required: true,
         },
     ],
     execute: async (logger, interaction, client) => {
-
-        const type = interaction.options.get("type").value;
-        const prompt = interaction.options.get("data").value;
         interaction.deferReply();
-        switch (type) {
-        case "personality":
-            if (message.member.permissions.has("Administrator")) {
-                try {
-                    await global.GuildManager.SetPersonality(message.guild, prompt);
-                    interaction.editReply({ content: 'Prompt successfully changed.'  });
-                } catch (err) {
-                    interaction.editReply({ content: `Error while changing the personality:\n${err}` , ephemeral: true });
-                }
-            }
-            break;
-        case "prompt": {
-            const conversationLog = [{ role: 'system', content: global.GuildManager.GetPersonality(interaction.guild).toString() }];
-            const configuration = new Configuration({
-                apiKey: process.env.OPENAI_API_KEY,
-            });
-            const openai = new OpenAIApi(configuration);
 
-            conversationLog.push({
-                role: 'user',
-                content: prompt,
-            });
+        try {
+            const prompt = `When responding to the following prompt, try to condense your response. Make sure it is under 2000 characters. Prompt: ${interaction.options.get("data").value}`;
+            const result = await got(`${process.env.PALM_API_PROXY_URL}?api_key=${process.env.PALM_API_KEY}&prompt=${encodeURIComponent(prompt)}`);
+            const response = JSON.parse(result.body).response;
+        
+            interaction.editReply(limitString(response, 2000));
 
-            const result = await openai
-                .createChatCompletion({
-                    model: 'gpt-3.5-turbo',
-                    messages: conversationLog,
-                    // max_tokens: 256, // limit token usage
-                })
-                .catch((error) => {
-                    logger.error(`OPENAI ERR: ${error}`);
-                });
+        } catch(err) {
+            SendErrorEmbed(interaction, "An error occured.", "red");
+        }
 
-            if (result.data.choices[0].message.content.length < 2000) {
-                await interaction.editReply({ content: result.data.choices[0].message.content, allowedMentions: { repliedUser: true } });
+        function limitString(string, limit) {
+            if (string.length <= limit) {
+                return string;
             } else {
-                const discriminator = CreateUniqueSeed(message);
-                await fs.writeFile(`./answer-${discriminator}.txt`, result.data.choices[0].message.content, { encoding: "utf8" });
-                await interaction.editReply({ files: [`./answer-${discriminator}.txt`] });
-                await fs.unlink(`./answer-${discriminator}.txt`);
+                return string.substring(0, limit - 3) + "...";
             }
         }
-        }
-
     }
 };
