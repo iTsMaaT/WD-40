d402dab8d557566fbfa0744c88f8924e59185975
iTsMaaT <matoulimar1@gmail.com>
2023-08-19 20:11:04 -0400

    fix?

--- Commands/slash/fun/fun/ask.js
+++ Commands/slash/fun/fun/ask.js
@@ -14,17 +14,17 @@ module.exports = {
         },
     ],
     execute: async (logger, interaction, client) => {
-        interaction.deferReply();
+        await interaction.deferReply();
 
         try {
             const prompt = `When responding to the following prompt, try to condense your response. Make sure it is under 2000 characters. Prompt: ${interaction.options.get("data").value}`;
             const result = await got(`${process.env.PALM_API_PROXY_URL}?api_key=${process.env.PALM_API_KEY}&prompt=${encodeURIComponent(prompt)}`);
             const response = JSON.parse(result.body).response;
         
-            interaction.editReply(limitString(response, 2000));
+            await interaction.editReply(limitString(response, 2000));
 
         } catch(err) {
-            SendErrorEmbed(interaction, "An error occured.", "red");
+            await SendErrorEmbed(interaction, "An error occured.", "red");
         }
 
         function limitString(string, limit) {
