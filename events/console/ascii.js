abd1c14c359f417e88fc0abd886a31e6a7d1fdc1
iTsMaaT <matoulimar1@gmail.com>
2023-11-10 17:01:41 -0500

    initial commit

---
+++ events/console/ascii.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "ascii",
+    execute(client, logger, args) {
+        if (!args[0]) return console.log("Argument needed");
+
+        const string = args;
+        let result = "";
+
+        for (word of string) {
+            console.log(word);
+            for (char of word) {
+                const asciiCode = char.charCodeAt(0);
+                // Convert ASCII code to binary
+                const binaryASCII = asciiCode.toString(2);
+
+                result += binaryASCII + " ";
+            }
+            result += "/ ";
+        }
+        console.log(result);
+    },
+};
