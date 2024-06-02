91774dad8ed3d0b047b0ab402ec16fc1c9d3b5e4
iTsMaaT <matoulimar1@gmail.com>
2024-06-02 10:23:55 -0400

    used api instead of reddit

--- Commands/text/fun/posts/cat.js
+++ Commands/text/fun/posts/cat.js
@@ -1,10 +1,24 @@
-const FetchReddit = require("@functions/FetchReddit.js");
+const { SendErrorEmbed } = require("@functions/discordFunctions");
 
 module.exports = {
     name: "cat",
     description: "Cats!",
     category: "posts",
     async execute(logger, client, message, args) {
-        message.reply({ embeds: [await FetchReddit(message.channel.nsfw, ["cat", "cats", "catswithjobs", "illegallysmolcats"])] });
+        try {
+            const url = await (await fetch("http://shibe.online/api/cats")).json();
+            const embed = {
+                color: 0xffffff,
+                title: "Enjoy!",
+                image: {
+                    url: url[0],
+                },
+                timestamp: new Date(),
+            };
+
+            message.reply({ embeds: [embed] });
+        } catch (err) {
+            return SendErrorEmbed(message, "Error fetching the image", "red");
+        }
     },
 };
