2c1dcd7d90a427c67023ea0d8e5814e1a9d8a5df
iTsMaaT <matoulimar1@gmail.com>
2024-07-07 16:49:11 -0400

    callback => promises

--- Commands/text/fun/fun/country.js
+++ Commands/text/fun/fun/country.js
@@ -10,50 +10,47 @@ module.exports = {
     },
     category: "fun",
     examples: ["Canada"],
-    execute(logger, client, message, args, optionalArgs) {
+    async execute(logger, client, message, args, optionalArgs) {
         if (!args[0]) return message.channel.send("No country provided");
-        
-        fetch(`https://restcountries.com/v3.1/name/${encodeURIComponent(args.slice(0).join(" "))}?fullText=true`)
-            .then(async response => {
-                const json = await response.json();
+        try {
 
-                if (json.status === "404") return SendErrorEmbed(message, "Couldn't find the specified country.", "red");
+            const json = await (await fetch(`https://restcountries.com/v3.1/name/${encodeURIComponent(args.slice(0).join(" "))}?fullText=true`)).json();
+            if (json.status === "404") return SendErrorEmbed(message, "Couldn't find the specified country.", "red");
 
-                const country = json[0];
-                const currencies = Object.values(country.currencies).map(currency => `${currency.name} (${currency.symbol})`);
-                const languages = Object.values(country.languages);
+            const country = json[0];
+            const currencies = Object.values(country.currencies).map(currency => `${currency.name} (${currency.symbol})`);
+            const languages = Object.values(country.languages);
 
-                const countryEmbed = {
-                    color: 0xffffff,
-                    title: country.name.common,
-                    thumbnail: {
-                        url: country.flags.png,
-                    },
-                    fields: [
-                        { name: "Top level domain name", value: country.tld[0], inline: true },
-                        { name: "Population", value: country.population, inline: true },
-                        { name: "Independant", value: country.independent + " (" + country.status + ")", inline: true },
-                        { name: "UN member", value: country.unMember, inline: true },
-                        { name: "Currency", value: currencies.join(", "), inline: true },
-                        { name: "Capital", value: country.capital[0], inline: true },
-                        { name: "Continent", value: country.continents[0], inline: true },
-                        { name: "Language(s)", value: languages.join(", ") },
-                        { name: "Flag info", value: country.flags.alt },
-                    ],
-                    timestamp: new Date(),
+            const countryEmbed = {
+                color: 0xffffff,
+                title: country.name.common,
+                thumbnail: {
+                    url: country.flags.png,
+                },
+                fields: [
+                    { name: "Top level domain name", value: country.tld[0], inline: true },
+                    { name: "Population", value: country.population, inline: true },
+                    { name: "Independant", value: country.independent + " (" + country.status + ")", inline: true },
+                    { name: "UN member", value: country.unMember, inline: true },
+                    { name: "Currency", value: currencies.join(", "), inline: true },
+                    { name: "Capital", value: country.capital[0], inline: true },
+                    { name: "Continent", value: country.continents[0], inline: true },
+                    { name: "Language(s)", value: languages.join(", ") },
+                    { name: "Flag info", value: country.flags.alt },
+                ],
+                timestamp: new Date(),
+            };
+
+            if (country.coatOfArms) {
+                countryEmbed.thumbnail = {
+                    url: country.coatOfArms.png,
                 };
+            }
 
-                if (country.coatOfArms) {
-                    countryEmbed.thumbnail = {
-                        url: country.coatOfArms.png,
-                    };
-                }
-
-                message.channel.send({ embeds: [countryEmbed] });
-            })
-            .catch(error => {
-                console.error("Error retrieving country information:", error);
-                return SendErrorEmbed(message, "Couldn't fetch country infomations", "red");
-            });
+            message.channel.send({ embeds: [countryEmbed] });
+        } catch (error) {
+            logger.error(`Error retrieving country information: ${error}`);
+            return SendErrorEmbed(message, "Couldn't fetch country infomations", "red");
+        }
     },
 };
--- Commands/text/fun/posts/meme.js
+++ Commands/text/fun/posts/meme.js
@@ -4,7 +4,6 @@ module.exports = {
     name: "meme",
     description: "Fetches a random post from a random shitpost sub",
     category: "posts",
-    cooldown: 10000,
     async execute(logger, client, message, args, optionalArgs) {
         message.reply({ embeds: [await FetchReddit(message.channel.nsfw, ["shitposting", "okmatecrosseur", "the_pack", "whenthe", "memes", "meme"], 5)] });
     },
