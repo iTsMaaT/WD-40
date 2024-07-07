03de8c42aee21cb214cb3d41ca656d6fed60d3cf
iTsMaaT <matoulimar1@gmail.com>
2024-07-07 16:11:14 -0400

    use multiple image embed function

--- Commands/text/utils/utils/mcskin.js
+++ Commands/text/utils/utils/mcskin.js
@@ -1,4 +1,4 @@
-const { SendErrorEmbed } = require("@functions/discordFunctions");
+const { SendErrorEmbed, multipleImageEmbed } = require("@functions/discordFunctions");
 
 module.exports = {
     name: "mcskin",
@@ -14,19 +14,16 @@ module.exports = {
         const name = args[0] ?? "mhf_steve";
     
         try {
-            const MCSkinEmbed = {
+            const embed = {
                 title: `Minecraft skin for **${args[0] ?? "Steve"}**`,
                 color: 0xffffff,
                 thumbnail: {
                     url: `https://mc-heads.net/avatar/${name}`,
                 },
-                image: {
-                    url: `https://mc-heads.net/skin/${name}`,
-                }, url: `https://mc-heads.net/user/${name}`,
                 timestamp: new Date(),
                 footer: { text: "Defaults to Steve if no arguments are provided" },
             };
-            message.reply({ embeds: [MCSkinEmbed, { image: { url: `https://mc-heads.net/body/${name}` }, url: `https://mc-heads.net/user/${name}` }] });
+            message.reply({ embeds: multipleImageEmbed(embed, `https://mc-heads.net/skin/${name}`, `https://mc-heads.net/user/${name}`, `https://mc-heads.net/body/${name}`) });
         } catch (err) {
             SendErrorEmbed(message, "An unknown error occured", "red");
             logger.error(err);
