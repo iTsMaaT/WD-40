ecfccef595ed0db56a241c6241d60e6572198fbb
iTsMaaT <matoulimar1@gmail.com>
2024-07-10 15:49:05 -0400

    cleaning

--- utils/functions/discordFunctions.js
+++ utils/functions/discordFunctions.js
@@ -1,4 +1,12 @@
+const { PermissionsBitField } = require("discord.js");
 const { prettyString } = require("@functions/formattingFunctions");
+
+/**
+ * Creates a webhook if it doesn't exist, or returns the existing one
+ * @param {Message} message The message object
+ * @param {string} name The name of the webhook
+ * @returns {webhook} The webhook
+ */
 const CreateOrUseWebhook = async function(message, name) {
     const webhooks = await message.channel.fetchWebhooks();
     
@@ -15,12 +23,24 @@ const CreateOrUseWebhook = async function(message, name) {
     return webhook;
 };
 
+/**
+ * Extracts the ID from a mention
+ * @param {string} mention The mention
+ * @returns {string} The ID
+ */
 const id = function(mention) {
     const cleanId = mention.replace(/[<!@>]/g, "");
     if (!cleanId.match(/^\d+$/)) return null;
     return cleanId;
 };
 
+/**
+ * Sends an error embed
+ * @param {Message} message The message object
+ * @param {string} string The error message
+ * @param {string} color The color of the embed
+ * @param {boolean} isSlash Whether the message is a slash command
+ */
 const SendErrorEmbed = async function(message, string, color, isSlash) {
     const embed = {
         title: prettyString(string.toString(), false, true),
@@ -42,6 +62,10 @@ const SendErrorEmbed = async function(message, string, color, isSlash) {
 
 /**
  * Reacts the provided string to a message
+ * @param {Client} client The client
+ * @param {string} ChannelID The channel ID
+ * @param {string} MessageID The message ID
+ * @param {string} string The string to react
  */
 const StringReact = function(client, ChannelID, MessageID, string) {
     const letters = string.toUpperCase().toString();
@@ -56,6 +80,11 @@ const StringReact = function(client, ChannelID, MessageID, string) {
     }
 };
 
+/**
+ * Extracts the information from a message link
+ * @param {string} link The message link
+ * @returns {Array} The information array
+ */
 const InfoFromMessageLink = function(link) {
     InfoArray = link.replace("https://discord.com/channels/", "").split("/");
     return InfoArray;
@@ -66,6 +95,12 @@ const InfoFromMessageLink = function(link) {
     */
 };
 
+/**
+ * Edits or sends a message
+ * @param {Channel} channel The channel object
+ * @param {string|object} messageOptions The message options
+ * @param {boolean} add Whether to add or edit
+ */
 async function editOrSend(channel, messageOptions, add) {
     if (!channel.id) throw new Error("Invalid channel");
 
@@ -103,6 +138,12 @@ async function editOrSend(channel, messageOptions, add) {
     }
 }
 
+/**
+ * Creates an embed with multiple images
+ * @param {object} embedOBJ The embed object
+ * @param {string} ...links The image links
+ * @returns {Array} The embed array
+ */
 function multipleImageEmbed(embedOBJ, ...links) {
     if (!links || links.length == 0) throw new Error("Image links missing");
     const mainLink = links[0];
@@ -114,6 +155,27 @@ function multipleImageEmbed(embedOBJ, ...links) {
     return [embedOBJ, ...embedParts];  
 }
 
+/**
+ * Gets the permission array names from a permission bitfield
+ * @param {number} flags The permission bitfield
+ * @returns {Array} The permission array names
+ */
+function getPermissionArrayNames(flags) {
+    const array = [];
+    const set = new Set(); // Use a Set to store unique values
+
+    for (const flag of flags) {
+        for (const [key, value] of Object.entries(PermissionsBitField.Flags)) {
+            if (value === flag && !set.has(key)) {
+                array.push(key);
+                set.add(key); // Add the key to the set to track uniqueness
+            }
+        }
+    }
+
+    return array;
+}
+
 module.exports = { 
     CreateOrUseWebhook,
     id,
@@ -122,4 +184,5 @@ module.exports = {
     InfoFromMessageLink,
     editOrSend, 
     multipleImageEmbed,
+    getPermissionArrayNames,
 };
