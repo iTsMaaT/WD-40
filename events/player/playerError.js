2994ad9130f4a51a5b5f682b9111586b4bc82e55
iTsMaaT <matoulimar1@gmail.com>
2024-07-24 18:25:54 -0400

    async and embedGenerator

--- events/player/playerError.js
+++ events/player/playerError.js
@@ -1,7 +1,10 @@
+const embedGenerator = require("@utils/helpers/embedGenerator");
+
 module.exports = {
     name: "playerError",
     once: false,
-    execute(client, logger, queue, error) {
+    async execute(client, logger, queue, error) {
+        await queue.metadata.channel.send({ embeds: [embedGenerator.error("An error occured, a track might have been skipped.")] });
         logger.info(`Queue: ${queue.metadata.guild.name} threw error: \n ${error}`);
     },
 };
