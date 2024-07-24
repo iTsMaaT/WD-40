4f0a2f9fbcd0cdc4a79d104e434273fe41fb59bc
iTsMaaT <matoulimar1@gmail.com>
2024-07-24 18:24:06 -0400

    added loop mode, paused status and live progress

--- Commands/text/music/nowplaying.js
+++ Commands/text/music/nowplaying.js
@@ -1,4 +1,5 @@
 const embedGenerator = require("@utils/helpers/embedGenerator");
+const { getLoopMode } = require("@utils/helpers/playerHelpers");
 const { useQueue, useTimeline, useMainPlayer } = require("discord-player");
 
 module.exports = {
@@ -17,9 +18,12 @@ module.exports = {
         const embed = embedGenerator.info({
             title: "Now Playing",
             description: `[${track.title}](${track.url})`,
+            thumbnail: { url: track.thumbnail },
             fields: [
                 { name: "Author", value: track.author },
-                { name: "Progress", value: track.isLive ? "Live ┃ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬🔘 ┃ Infinity (99%)" : `${queue.node.createProgressBar()} (${timeline.timestamp?.progress}%)` },    
+                { name: "Progress", value: track.live ? "Live ┃ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬🔘 ┃ Infinity (99%)" : `${queue.node.createProgressBar()} (${timeline.timestamp?.progress}%)` }, 
+                { name: "Loop mode", value: getLoopMode(queue), inline: true },
+                { name: "Paused", value: timeline.paused ? "Yes" : "No", inline: true },
                 { name: "Extractor", value: `\`${track.extractor?.identifier || "N/A"}\`` },
             ],
             footer: { text: `Event Loop Lag: ${queue.player.eventLoopLag.toFixed(0)}ms` },
