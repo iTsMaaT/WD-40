5ea1c444322e38fba2af4dae577cdcd6e27c6148
iTsMaaT <matoulimar1@gmail.com>
2024-07-24 18:24:35 -0400

    rework

--- Commands/text/utils/owner/activity.js
+++ Commands/text/utils/owner/activity.js
@@ -9,34 +9,39 @@ module.exports = {
         required: {
             "activity": "activity to change to",
         },
+        optional: {
+            "list|l": {
+                hasValue: false,
+                description: "Lists all the available activities",
+            },
+            "preset|p": {
+                hasValue: true,
+                description: "Changes the activity to a preset",
+            },
+        },
     },
     private: true,
-    execute(logger, client, message, args, optionalArgs) {
-        // Changes the activity or gives a list of all presets
-
-        if (!args[0]) {
+    async execute(logger, client, message, args, optionalArgs) {
+        if (!args[0] && !optionalArgs["preset|p"] && !optionalArgs["list|l"]) {
             client.user.setActivity(activities[Math.floor(Math.random() * activities.length)], { type: ActivityType.Custom });
-            message.reply({ content: "Activity randomised" });
-            return;
+            return await message.reply({ content: "Activity randomised" });
         }
-        switch (args[0]) {
-            case "-l": {
-                let activityList = "";
-                const maxIndexWidth = (activities.length - 1).toString().length;
-                activities.forEach((activity, index) => {
-                    const formattedIndex = `[${index.toString().padStart(maxIndexWidth, " ")}]`;
-                    activityList += `${formattedIndex} : ${activity}\n`;
-                });
-                message.reply({ content: `\`\`\`${activityList}\`\`\`` });
-                break;
-            }
-            case "-p":
-                client.user.setActivity(activities[args[1]], { type: ActivityType.Custom });
-                message.reply({ content: `Activity changed to : \`${activities[args[1]]}\``  });
-                break;
-            default:
-                client.user.setActivity(args.join(" "), { type: ActivityType.Custom });
-                message.reply({ content: "Activity updated"  });
+
+        if (optionalArgs["list|l"]) {
+            let activityList = "";
+            const maxIndexWidth = (activities.length - 1).toString().length;
+            activities.forEach((activity, index) => {
+                const formattedIndex = `[${index.toString().padStart(maxIndexWidth, " ")}]`;
+                activityList += `${formattedIndex} : ${activity.name}\n`;
+            });
+            return await message.reply({ content: `\`\`\`${activityList}\`\`\`` });
         }
+
+        if (optionalArgs["preset|p"]) {
+            client.user.setActivity(activities[optionalArgs["preset|p"]], { type: ActivityType.Custom });
+            return await message.reply({ content: `Activity changed to : \`${activities[optionalArgs["preset|p"]]}\``  });
+        }
+
+        await message.reply({ embeds: [embedGenerator.warning("Invalid activity")] });
     },
 };
