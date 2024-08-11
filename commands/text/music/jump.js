5f80366b32fb17dc945c577e811eeaf333d20eee
iTsMaaT <matoulimar1@gmail.com>
2024-08-11 10:45:06 -0400

    works better

--- commands/text/music/jump.js
+++ commands/text/music/jump.js
@@ -14,30 +14,24 @@ module.exports = {
     inVoiceChannel: true,
     execute: async (logger, client, message, args, optionalArgs) => {
         const queue = useQueue(message.guild.id);
-        const track = parseInt(args[0]);
-        const jump = queue?.tracks.at(track);
-        const position = queue?.node.getTrackPosition(jump);
-        const tracks = queue?.tracks.map((song, id) => ({
-            name: song.title,
-            value: ++id,
-        }));
 
-        if (jump?.title && !tracks.some((t) => t.name === jump.title)) {
-            tracks.unshift({
-                name: jump.title,
-                value: position,
-            });
-        }
+        if (!message.member.voice.channel) return await message.reply({ embeds: [embedGenerator.warning("You must be in a voice channel.")] });
+        if (!args[0]) return await message.reply({ embeds: [embedGenerator.error("Please enter a number to skip to.")] });
+
+        const tojump = parseInt(args[0]);
+
+        if (tojump > queue.tracks.data.length) return await message.reply({ embeds: [embedGenerator.error("Please enter a number between 1 and " + queue.tracks.data.length)] });
 
         if (!queue || !queue.tracks) return await message.reply({ embeds: [embedGenerator.error("There is nothing in the queue.")] });
 
-        if (!args[0]) return await message.reply({ embeds: [embedGenerator.error("Please enter a number to skip to.")] });
+        const trackResolvable = queue.tracks.at(tojump - 1);
 
-        const trackResolvable = queue.tracks.at(jump);
-
-        if (isNaN(track) || !trackResolvable) return await message.reply({ embeds: [embedGenerator.error("Couldn't find song to skip to.")] });
+        if (!trackResolvable) return await message.reply({ embeds: [embedGenerator.error("Couldn't find song to skip to.")] });
 
         queue.node.jump(trackResolvable);
-        return await message.reply({ embeds: [embedGenerator.info(`Skiped to ${trackResolvable.title}`)] });
+        return await message.reply({ embeds: [embedGenerator.info({ 
+            title: "Skipped to",
+            description: `[${trackResolvable.title}](${trackResolvable.url})`,
+        })] });
     },
 };
