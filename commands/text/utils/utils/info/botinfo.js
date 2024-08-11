c9540b723b66c95abbac9d869d1883a23730d956
iTsMaaT <matoulimar1@gmail.com>
2024-08-11 10:47:01 -0400

    process usage added, and dev env check

--- commands/text/utils/utils/info/botinfo.js
+++ commands/text/utils/utils/info/botinfo.js
@@ -1,5 +1,6 @@
 const GetPterodactylInfo = require("@functions/GetPterodactylInfo");
 const embedGenerator = require("@utils/helpers/embedGenerator");
+const { ToEngineerNotation } = require("@functions/formattingFunctions");
 
 module.exports = {
     name: "botinfo",
@@ -9,12 +10,26 @@ module.exports = {
     async execute(logger, client, message, args, optionalArgs) {
         try {
             const PteroInfo = await GetPterodactylInfo();
+            const memoryUsage = process.memoryUsage();
+            let memoryUsageString = "";
+            const rss = ToEngineerNotation(memoryUsage.rss) + "B";
+            const heapTotal = ToEngineerNotation(memoryUsage.heapTotal) + "B";
+            const heapUsed = ToEngineerNotation(memoryUsage.heapUsed) + "B";
+            const external = ToEngineerNotation(memoryUsage.external) + "B";
+            const arrayBuffers = ToEngineerNotation(memoryUsage.arrayBuffers) + "B";
+
+            memoryUsageString += `RSS: ${rss}\n`;
+            memoryUsageString += `Heap Total: ${heapTotal}\n`;
+            memoryUsageString += `Heap Used: ${heapUsed}\n`;
+            memoryUsageString += `External: ${external}\n`;
+            memoryUsageString += `Array Buffers: ${arrayBuffers}`;
+            
             const embed = {
-                title: `Pterodactyl info for ${PteroInfo.main.name}`,
+                title: `${process.env.SERVER == "prod" ? "Pterodactyl and" : ""} Process info for ${PteroInfo.main.name}`,
                 color: 0xffffff,
                 description: `Uptime: ${PteroInfo.uptime.clean}`,
-                fields: [
-                    {
+                fields: process.env.SERVER == "prod" ?
+                    [{
                         name: "RAM usage",
                         value: `${PteroInfo.ram.usage.clean} / ${PteroInfo.ram.limit.clean} (${PteroInfo.ram.pourcentage.clean})`,
                     }, {
@@ -26,13 +41,18 @@ module.exports = {
                     }, {
                         name: "Network",
                         value: `IN: ${PteroInfo.network.download.clean}\nOUT: ${PteroInfo.network.upload.clean}`,
-                    },
-                ],
+                    }] : [],
                 footer: {
                     text: "",
                 },
                 timestamp: new Date(),
             };
+
+            embed.fields.push({
+                name: "Process memory usage",
+                value: memoryUsageString,
+            });
+
             message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
         } catch (err) {
             logger.error(err);
