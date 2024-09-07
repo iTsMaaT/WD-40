c7d89a4e1bb65125f5942edded47ef56dcfeef75
iTsMaaT <matoulimar1@gmail.com>
2024-09-07 12:55:30 -0400

    s

--- utils/helpers/isURL.js
+++ utils/helpers/isURL.js
@@ -1,5 +1,5 @@
 /**
- * imple function to check if a string is a valid URL
+ * simple function to check if a string is a valid URL
  * @param {string} string The string to check
  * @returns {boolean} True if the string is a valid URL, false otherwise
  */
