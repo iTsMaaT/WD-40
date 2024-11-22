4acea4ed4124fd3dc44657134236e19ff5edd1f7
iTsMaaT <matoulimar1@gmail.com>
2024-11-22 15:23:46 -0500

    change channel

--- events/client/guildCreate.js
+++ events/client/guildCreate.js
@@ -10,7 +10,7 @@ module.exports = {
         const guildblacklist = config.get("guildBlacklist");
         await GuildManager.SetActiveOrCreate(guild);
         logger.info(`The bot has been added to \`${guild.name}\``);
-        client.channels.cache.get(process.env.MEMBERS_UPDATE_ID).send(`The bot has been added to \`${guild.name}\``);
+        client.channels.cache.get(process.env.GUILD_UPDATE_ID)?.send(`The bot has been added to \`${guild.name}\``);
         if (guildblacklist.includes(guild.id)) await guild.leave();
     },
 };
--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -7,14 +7,18 @@ module.exports = {
     once: false,
     log: true,
     async execute(client, logger, guild) {
-        const DBguildIDs = (await repositories.guildsettings.select()).map(item => item.guildId);
-        const botGuildIds = client.guilds.cache.map(gui => gui.id);
-        const notInGuildIds = DBguildIDs.filter(id => !botGuildIds.includes(id));
-        for (const notInGuildId of notInGuildIds) 
-            await GuildManager.SetActiveOrCreate({ id: notInGuildId }, false);
+        try {
+            const DBguildIDs = (await repositories.guildsettings.select()).map(item => item.guildId);
+            const botGuildIds = client.guilds.cache.map(gui => gui.id);
+            const notInGuildIds = DBguildIDs.filter(id => !botGuildIds.includes(id));
+            for (const notInGuildId of notInGuildIds) 
+                await GuildManager.SetActiveOrCreate({ id: notInGuildId }, false);
+        } catch (error) {
+            logger.error("Error while deleting guild from database: " + error);
+        }
         
         if (!guild.available || !guild.name) return;
         logger.info(`The bot has been removed from \`${guild.name}\``);
-        client.channels.cache.get(process.env.MEMBERS_UPDATE_ID).send(`The bot has been removed from \`${guild.name}\``);
+        client.channels.cache.get(process.env.GUILD_UPDATE_ID)?.send(`The bot has been removed from \`${guild.name}\``);
     },
 };
