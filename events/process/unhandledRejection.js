2ce10e6f86e06a84b487f0da0aa65ccc0bf2db0d
iTsMaaT <matoulimar1@gmail.com>
2025-01-01 11:30:04 -0500

    warning logging on missing permissions

--- events/process/unhandledRejection.js
+++ events/process/unhandledRejection.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "unhandledRejection",
     async execute(client, logger, err, promise) {
+        if (err.includes("DiscordAPIError[50013]: Missing Permissions")) 
+            return logger.warning("Missing permissions error occured, igoring stack.");
         logger.event("Unhandled Promise Rejection:");
         logger.severe(err);
     },
