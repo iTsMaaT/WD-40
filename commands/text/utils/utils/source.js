21527c5c7b04157ff15f41326dfdc24e653a4086
iTsMaaT <matoulimar1@gmail.com>
2025-01-05 20:32:19 -0500

    shows full command path

--- commands/text/utils/utils/source.js
+++ commands/text/utils/utils/source.js
@@ -1,28 +1,28 @@
 const SplitIntoChunks = require("@root/utils/functions/splitIntoChunks");
 const embedGenerator = require("@utils/helpers/embedGenerator");
+const { repository } = require("@root/package.json");
 
 module.exports = {
     name: "source",
     description: "Gives the source code of a command",
     category: "utils",
     async execute(logger, client, message, args, optionalArgs) {
-        if (message.author.id == process.env.OWNER_ID && args[0]) {
+        if (args[0]) {
             const commandName = args[0];
             const command = client.commands.get(commandName);
 
-            if (!command) return message.channel.send(`Command \`${commandName}\` not found.`);
+            if (!command) return message.reply({ embeds: [embedGenerator.error("Command not found.")] });
+            
+            const filePath = command.filePath;
 
-            const code = command.execute.toString();
-            const chunks = SplitIntoChunks(code);
-
-            await message.reply(`Code for command \`${commandName}\``);
-
-            for (const chunk of chunks) 
-                await message.channel.send(`\`\`\`javascript\n${chunk}\n\`\`\``);
+            await message.reply({ embeds: [embedGenerator.info({
+                title: "GitHub link",
+                description: repository.url.replace(/git\+|\.git/g, "") + "/blob/production/commands" + filePath.split("commands")[1],
+            }).withAuthor(message.author)] });
         } else {
             await message.reply({ embeds: [embedGenerator.info({
                 title: "GitHub link",
-                description: "https://github.com/iTsMaaT/WD-40",
+                description: repository.url.replace(/git\+|\.git/g, ""),
             }).withAuthor(message.author)] });
         }
     },
