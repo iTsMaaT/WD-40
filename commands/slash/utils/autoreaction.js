71b3a32ca53332ab603723d3a178f862fffa8446
iTsMaaT <matoulimar1@gmail.com>
2025-01-05 20:29:12 -0500

    fix

--- commands/slash/utils/autoreaction.js
+++ commands/slash/utils/autoreaction.js
@@ -29,7 +29,7 @@ module.exports = {
                     max_length: 100,
                 }, {
                     name: "emotes",
-                    description: "The reactions (unicode emotes are preferred)",
+                    description: "The reactions",
                     type: ApplicationCommandOptionType.String,
                     required: true,
                 },
@@ -140,8 +140,6 @@ module.exports = {
                 break;
             }
             case "add": {
-                console.log("executed");
-
                 if (Object.keys(reactions).length >= 20) return await interaction.editReply({ embeds: [embedGenerator.warning("You cannot have more than 20 auto-reactions")], ephemeral: true });
 
                 if (reactions[ChannelPromptInput]) {
