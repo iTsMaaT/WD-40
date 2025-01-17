6bcf400ab7ebf0462d0d58d464e53d79dee8bb83
iTsMaaT <matoulimar1@gmail.com>
2025-01-17 16:32:01 -0500

    refactor userinfo command: enhance activity status handling and improve embed details

--- commands/text/utils/utils/info/userinfo.js
+++ commands/text/utils/utils/info/userinfo.js
@@ -1,5 +1,6 @@
 const embedGenerator = require("@utils/helpers/embedGenerator");
 const { prettyString } = require("@functions/formattingFunctions");
+const { ActivityType } = require("discord.js");
 
 module.exports = {
     name: "userinfo",
@@ -43,16 +44,39 @@ module.exports = {
             }
         }
 
-        let custom_status, activity_name, activity_details;
-        try {
-            custom_status = status?.activities[0]?.state ?? "`No status`";
-            activity_name = status?.activities[1]?.name ?? "`No activity name`";
-            activity_details = status?.activities[1]?.details ?? "`No activity details`";
-        } catch {
-            custom_status = "-";
-            activity_name = "-";
-            activity_details = "-";
+        console.log(status?.activities);
+
+        const status_type = status?.activities[0]?.type ?? null;
+        const custom_status = status?.activities[0]?.state ?? status?.activities[0]?.name ?? "`No status`";
+        const activity_name = status?.activities[1]?.name ?? "`No activity name`";
+        const activity_details = status?.activities[1]?.details ?? "`No activity details`";
+        let status_type_name;
+        
+        switch (status_type) {
+            case ActivityType.Custom:
+                status_type_name = "";
+                break;
+            case ActivityType.Playing:
+                status_type_name = "**Playing **";
+                break;
+            case ActivityType.Listening:
+                status_type_name = "**Listening **";
+                break;
+            case ActivityType.Competing:
+                status_type_name = "**Competing **";
+                break;
+            case ActivityType.Streaming:
+                status_type_name = "**Streaming **";
+                break;
+            case ActivityType.Watching:
+                status_type_name = "**Watching **";
+                break;
+            default:
+                status_type_name = "";
+                break;
         }
+
+        const highest_role = target?.roles?.highest ? `<@&${target.roles.highest.id}>` : null;
 
         try {
             const FullClientStatus = target?.presence?.clientStatus;
@@ -60,8 +84,8 @@ module.exports = {
             for (const key in FullClientStatus) {
                 if (Object.prototype.hasOwnProperty.call(FullClientStatus, key)) 
                     ClientSatus += `${prettyString(key, "first", false)}: \`${prettyString(FullClientStatus[key], "first", false)}\n\``;
-                
             }
+            
             const userInfoEmbed = {
                 title: target.displayName,
                 description: `User Information For: <@${target.id}>`,
@@ -73,10 +97,10 @@ module.exports = {
                     { name: "Status", value: ClientSatus || "Offline" },
                     { name: "Account Age", value: `${target?.user?.createdTimestamp ? `<t:${parseInt(target?.user?.createdTimestamp / 1000)}:R>` : "-"}` },
                     { name: "Member Since", value: `${target.joinedTimestamp ? `<t:${parseInt(target?.joinedTimestamp / 1000)}:R>` : "-"}` },
-                    { name: "Custom Status", value: custom_status || "-" },
+                    { name: "Custom Status", value: status_type_name + custom_status || "-" },
                     { name: "Activity Title", value: activity_name || "-" },
                     { name: "Activity Details", value: activity_details || "-" },
-                    { name: "Highest Role", value: target.roles?.highest?.name || "-" },
+                    { name: "Highest Role", value: highest_role || "-" },
                 ],
                 timestamp: new Date(),
                 color: 0xffffff,
