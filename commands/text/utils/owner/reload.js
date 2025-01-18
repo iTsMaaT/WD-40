496027615725d1c2aaf7dec23901a549e753c05f
iTsMaaT <matoulimar1@gmail.com>
2025-01-18 12:49:48 -0500

    feat(reload): add command to reload the configuration

---
+++ commands/text/utils/owner/reload.js
@@ -0,0 +1,13 @@
+const config = require("@utils/config/configUtils");
+const embedGenerator = require("@utils/helpers/embedGenerator");
+
+module.exports = {
+    name: "reload",
+    description: "Reload the config",
+    category: "owner",
+    private: true,
+    async execute(logger, client, message, args, optionalArgs) {
+        config.reload();
+        await message.reply({ embeds: [embedGenerator.success("Reloaded the config.")] });
+    },
+};
