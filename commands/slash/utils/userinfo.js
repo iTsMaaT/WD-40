1ca8cdeef0b04616ebb80e7d37fe7f1886b77829
iTsMaaT <matoulimar1@gmail.com>
2025-02-26 20:59:31 -0500

    better

--- commands/slash/utils/userinfo.js
+++ commands/slash/utils/userinfo.js
@@ -1,4 +1,6 @@
-const { ApplicationCommandType, ApplicationCommandOptionType } = require("discord.js");
+const { ApplicationCommandType, ApplicationCommandOptionType, ActivityType } = require("discord.js");
+const embedGenerator = require("@utils/helpers/embedGenerator");
+const { prettyString } = require("@functions/formattingFunctions");
 
 module.exports = {
     name: "userinfo",
@@ -15,8 +17,8 @@ module.exports = {
     execute: async (logger, interaction, client) => {
         const guild = await client.guilds.fetch(interaction.guildId);
         const user = interaction.options.getUser("user");
-        const id = user || interaction.user.id;
-        
+        const id = user ? user.id : interaction.user.id;
+
         let target;
         let status;
 
@@ -29,51 +31,80 @@ module.exports = {
                     target = await client.users.fetch(id);
                     status = null; // No presence information available
                 } catch (err) {
-                    console.log(err);
-                    await interaction.editReply({ embeds: [embedGenerator.error("User not found")] });
+                    logger.log(err);
+                    return await interaction.editReply({ embeds: [embedGenerator.error("User not found.")] });
                 }
             } else {
-                console.log(e);
-                return await interaction.editReply({ embeds: [embedGenerator.error("An error occured.")] });
+                logger.error(e);
+                return await interaction.editReply({ embeds: [embedGenerator.error("An error occurred while fetching the user.")] });
             }
         }
 
-        let custom_status, activity_name, activity_details;
-        try {
-            custom_status = status?.activities[0]?.state ?? "`No status`";
-            activity_name = status?.activities[1]?.name ?? "`No activity name`";
-            activity_details = status?.activities[1]?.details ?? "`No activity details`";
-        } catch {
-            custom_status = "-";
-            activity_name = "-";
-            activity_details = "-";
+        const status_type = status?.activities[0]?.type ?? null;
+        const custom_status = status?.activities[0]?.state ?? status?.activities[0]?.name ?? "`No status`";
+        const activity_name = status?.activities[1]?.name ?? "`No activity name`";
+        const activity_details = status?.activities[1]?.details ?? "`No activity details`";
+        let status_type_name;
+
+        switch (status_type) {
+            case ActivityType.Custom:
+                status_type_name = "";
+                break;
+            case ActivityType.Playing:
+                status_type_name = "**Playing **";
+                break;
+            case ActivityType.Listening:
+                status_type_name = "**Listening **";
+                break;
+            case ActivityType.Competing:
+                status_type_name = "**Competing **";
+                break;
+            case ActivityType.Streaming:
+                status_type_name = "**Streaming **";
+                break;
+            case ActivityType.Watching:
+                status_type_name = "**Watching **";
+                break;
+            default:
+                status_type_name = "";
+                break;
         }
 
+        const highest_role = target?.roles?.highest ? `<@&${target.roles.highest.id}>` : null;
+
         try {
-            const presenceStatus = target?.presence?.status || "Offline";
+            const FullClientStatus = target?.presence?.clientStatus;
+            let ClientSatus = "";
+            for (const key in FullClientStatus) {
+                if (Object.prototype.hasOwnProperty.call(FullClientStatus, key))
+                    ClientSatus += `${prettyString(key, "first", false)}: \`${prettyString(FullClientStatus[key], "first", false)}\n\``;
+            }
+
             const userInfoEmbed = {
-                title: "User Information",
+                title: target.displayName,
                 description: `User Information For: <@${target.id}>`,
                 thumbnail: {
                     url: target.avatarURL({ dynamic: true }) || "",
                 },
                 fields: [
-                    { name: "User ID", value: target.id },
-                    { name: "Status", value: presenceStatus },
-                    { name: "Account Age", value: `<t:${parseInt(target.user.createdTimestamp / 1000)}:R>` },
-                    { name: "Member Since", value: `${target.joinedTimestamp ? `<t:${parseInt(target.joinedTimestamp / 1000)}:R>` : "-"}` },
-                    { name: "Custom Status", value: custom_status },
-                    { name: "Activity Title", value: activity_name },
-                    { name: "Activity Details", value: activity_details },
-                    { name: "Highest Role", value: target.roles?.highest?.name || "-" },
+                    { name: "User ID", value: target.id || "-" },
+                    { name: "Status", value: ClientSatus || "Offline" },
+                    { name: "Account Age", value: `${target?.user?.createdTimestamp ? `<t:${parseInt(target?.user?.createdTimestamp / 1000)}:R>` : "-"}` },
+                    { name: "Member Since", value: `${target.joinedTimestamp ? `<t:${parseInt(target?.joinedTimestamp / 1000)}:R>` : "-"}` },
+                    { name: "Custom Status", value: status_type_name + custom_status || "-" },
+                    { name: "Activity Title", value: activity_name || "-" },
+                    { name: "Activity Details", value: activity_details || "-" },
+                    { name: "Highest Role", value: highest_role || "-" },
                 ],
                 timestamp: new Date(),
                 color: 0xffffff,
             };
+            console.log("yes");
 
-            await interaction.editReply({ embeds: [userInfoEmbed]  });
+            await interaction.reply({ embeds: [userInfoEmbed] });
         } catch (err) {
-            await interaction.editReply({ embeds: [embedGenerator.error("An error occured.")] });
+            logger.error(err);
+            return await interaction.reply({ embeds: [embedGenerator.error("An error occured.")] });
         }
     },
 };
