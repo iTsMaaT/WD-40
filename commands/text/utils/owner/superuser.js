fa968dba58eda4174ffef948db70924b638c098e
iTsMaaT <matoulimar1@gmail.com>
2025-02-26 21:09:16 -0500

    use config instead of env

--- commands/text/utils/owner/superuser.js
+++ commands/text/utils/owner/superuser.js
@@ -1,4 +1,5 @@
 const embedGenerator = require("@utils/helpers/embedGenerator");
+const config = require("@utils/config/configUtils");
 
 module.exports = {
     name: "superuser",
@@ -11,13 +12,11 @@ module.exports = {
     },
     private: true,
     async execute(logger, client, message, args, optionalArgs) {
-        // Superuser command (Only iTsMaaT can execute commands)
-        const server = process.env.SERVER;
-        if (args[0] == server) {
-            process.env.CURRENT_SUPERUSER_STATE = !process.env.CURRENT_SUPERUSER_STATE;
-            return await message.reply({ embeds: [
-                embedGenerator.success(`Superuser state set to ${process.env.CURRENT_SUPERUSER_STATE ? "enabled" : "disabled"}`),
-            ] });
-        }
+        config.set("defaultSuperuserState", !config.get("defaultSuperuserState"));
+        return await message.reply({
+            embeds: [
+                embedGenerator.success(`Superuser state set to ${config.get("defaultSuperuserState") ? "enabled" : "disabled"}`),
+            ],
+        });
     },
 };
