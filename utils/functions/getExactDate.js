6afb6cf977a2aea43d5458474539fd04058b177b
iTsMaaT <matoulimar1@gmail.com>
2025-03-10 20:32:27 -0400

    Enhance getExactDate function to format date using configured locale

--- utils/functions/getExactDate.js
+++ utils/functions/getExactDate.js
@@ -1,13 +1,28 @@
+const config = require("../config/configUtils");
+
 /**
- * Get the exact date and time
+ * Get the exact date and time using the configured locale
  * @returns {string} The exact date and time
  */
 const getExactDate = function() {
     const today = new Date();
-    const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0");
-    const yyyy = today.getFullYear();
-    const time = String(today.getHours()).padStart(2, "0") + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0") + "." + String(today.getMilliseconds()).padStart(3, "0");
-    return dd + "-" + mm + "-" + yyyy + " " + time;
+    const locale = config.get("locale") || "en-US";
+    
+    // Format date using locale
+    const dateFormatter = new Intl.DateTimeFormat(locale, {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        fractionalSecondDigits: 3,
+        hour12: false,
+    });
+
+    return dateFormatter.format(today)
+        .replace(/[/]/g, "-")
+        .replace(",", "");
 };
+
 module.exports = getExactDate;
