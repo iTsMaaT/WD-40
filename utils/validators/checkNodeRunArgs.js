ba27319e50c24e02673411ff77f0e07dea357957
iTsMaaT <matoulimar1@gmail.com>
2025-04-16 20:21:48 -0400

    Update error message for missing exec argument in checkNodeRunArgs function

--- utils/validators/checkNodeRunArgs.js
+++ utils/validators/checkNodeRunArgs.js
@@ -1,7 +1,7 @@
 const logger = require("@utils/log");
 
 /**
- * Checks if the proper arguments are passed to the node.js run command+
+ * Checks if the proper arguments are passed to the node.js run command
  * @returns {void}
  */
 const checkNodeRunArgs = async function() {
@@ -9,7 +9,7 @@ const checkNodeRunArgs = async function() {
     const neededArgs = ["--openssl-legacy-provider"];
     for (const arg of neededArgs) {
         if (!args.includes(arg)) {
-            logger.severe(`The following argument is missing: ${arg}`);
+            logger.severe(`The following exec argument is missing: ${arg}`);
             process.exit(0);
         }
     }
