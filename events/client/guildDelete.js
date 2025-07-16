cb5fa69a01f1418bfde588ddba81c513c1bcd533
iTsMaaT <matoulimar1@gmail.com>
2025-07-16 15:40:57 -0400

    Proper checks for optional env vars

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -1,20 +1,23 @@
 const { Events } = require("discord.js");
 const GuildManager = require("@guildManager");
 const { repositories } = require("@utils/db/tableManager.js");
+const dbManager = require("@utils/db/databaseManager");
 
 module.exports = {
     name: Events.GuildDelete,
     once: false,
     log: true,
     async execute(client, logger, guild) {
-        try {
-            const DBguildIDs = (await repositories.guildsettings.select()).map(item => item.guildId);
-            const botGuildIds = client.guilds.cache.map(gui => gui.id);
-            const notInGuildIds = DBguildIDs.filter(id => !botGuildIds.includes(id));
-            for (const notInGuildId of notInGuildIds) 
-                await GuildManager.SetActiveOrCreate({ id: notInGuildId }, false);
-        } catch (error) {
-            logger.error("Error while deleting guild from database: " + error);
+        if (dbManager.dbExists()) {
+            try {
+                const DBguildIDs = (await repositories.guildsettings.select()).map(item => item.guildId);
+                const botGuildIds = client.guilds.cache.map(gui => gui.id);
+                const notInGuildIds = DBguildIDs.filter(id => !botGuildIds.includes(id));
+                for (const notInGuildId of notInGuildIds) 
+                    await GuildManager.SetActiveOrCreate({ id: notInGuildId }, false);
+            } catch (error) {
+                logger.error("Error while deleting guild from database: " + error);
+            }
         }
         
         if (!guild.available || !guild.name) return;
