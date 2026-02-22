8f3d997b7fcbe3c4084c1dab7dc29bb75d1c7477
Antoine Delisle <23488509+KrazyPotatto@users.noreply.github.com>
2026-02-21 22:08:35 -0500

    Revert "not receiving suggestions anymore"
    This reverts commit 5b90ce82d6855b12484baacb7517ffa99c4d5d81.

--- commands/text/utils/utils/suggestion.js
+++ commands/text/utils/utils/suggestion.js
@@ -3,8 +3,23 @@ const embedGenerator = require("@utils/helpers/embedGenerator");
 module.exports = {
     name: "suggestion",
     description: "Suggest ideas for the bot",
+    usage: {
+        required: {
+            "suggestion": "The suggestion you want to send",
+        },
+    },
     category: "utils",
     async execute(logger, client, message, args, flags) {
-        message.reply({ embeds: [embedGenerator.info("To give a suggestion, you can join the support server by doing >help")] });
+
+        if (!process.env.SUGGESTION_CHANNEL_ID) return await message.reply({ embeds: [embedGenerator.error("No suggestion channel has been set by the owner.")] });
+
+        const channel = await client.channels.fetch(process.env.SUGGESTION_CHANNEL_ID);
+        const sent = await channel.send(`**Suggestion by ${message.author.displayName} (<@${message.author.id}>) received: **` + args.join(" "));
+        await channel.send("===================");
+        await sent.react("🔴");
+        await sent.react("🟢");
+        await sent.react("✅");
+        logger.info("Suggestion received");
+        await message.reply({ embeds: [embedGenerator.info("Your suggestion has been sent.\nFor a bug report, you can join the support server by doing >help")] });
     },
 };
